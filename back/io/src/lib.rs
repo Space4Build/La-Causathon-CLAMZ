2ef475cc970502fb0175d65e1b1bb36a54fb85d3
OLDSPICECODE <56983980+OLDSPICECODE@users.noreply.github.com>
2024-06-23 14:02:02 -0500

    lib.rs

--- back/io/src/lib.rs
+++ back/io/src/lib.rs
@@ -6,38 +6,52 @@ use gmeta::{Out,InOut,Metadata};
 
 // 1. Create your own Actions
 #[derive(Encode, Decode, TypeInfo,  Clone)]
-pub enum TrafficLightAction {
+pub enum AIPipeliningAction {
     Enable,
     Disable,
-    AddQuery(Vec<String>)
+    RegisterUser,
+    AddQuery(Vec<String>),
+    AddScore(String),
+    AddOutput(Vec<String>),
+    AIValidation(ActorId)
 }
 
 
 // 2. Create your own Events
 #[derive(Encode, Decode, TypeInfo)]
-pub enum TrafficLightEvent {
+pub enum AIPipeliningEvent {
     Enable,
     Disable,
-    ReplyQuery(Vec<String>)
+    ReplyQuery(Vec<String>),
+    ScoreAdded,
+    UserRegistered,
+    OutputReceived,
+    ActorValid
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum Errors {
+   NotUser
 }
 
 // 3. Create your own Struct
 #[derive(Default, Clone, Encode, Decode, TypeInfo)]
-pub struct IoTrafficLightState {
+pub struct IoAIPipeliningState {
     pub current_light: String,
-    pub all_users: Vec<(ActorId, String)>,
-    pub querys:Vec<(ActorId, Vec<String>)>
+    pub users: Vec<ActorId>,
+    pub querys:Vec<(ActorId, Vec<String>)>,
+    pub current_score: String,
+    pub outputs:Vec<(ActorId, Vec<String>)>,
 }
-
 
 pub struct ContractMetadata;
 
 // 5. Define the structure of actions, events and state for your metadata.
 impl Metadata for ContractMetadata{
      type Init = ();
-     type Handle = InOut<TrafficLightAction,TrafficLightEvent>;
+     type Handle = InOut<AIPipeliningAction,Result<AIPipeliningEvent,Errors>>;
      type Others = ();
      type Reply=();
      type Signal = ();
-     type State = Out<IoTrafficLightState>;
+     type State = Out<IoAIPipeliningState>;
 }
